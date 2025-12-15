71ae72a31074312dbedf09d7e5ff298bb52c6522
Benita Almroth <benita.almroth@hotmail.com>
2025-12-15 10:44:24 +0100

    added room info to database, fetched info to rooms.php

--- app/bookings/rooms.php
+++ app/bookings/rooms.php
@@ -1,1 +1,21 @@
-<!-- alla rum och dess info anropa från db -->
+<?php require __DIR__ . '/../autoload.php'; ?>
+
+<?php 
+
+$statement = $database->query('SELECT * FROM rooms ORDER BY id');
+
+$rooms = $statement->fetchAll(PDO::FETCH_ASSOC); ?>
+
+<?php
+
+    foreach ($rooms as $room) { ?>
+        
+        <div class="room">
+            <h3><?= $room['name']; ?></h3>
+            <h4><?= $room['tier']; ?></h4>
+            <h5><?= 'cost:'.' '.$room['cost'].' '.'Bells'; ?></h5>
+            <p><?= $room['description']; ?></p>
+        </div>
+        
+    <?php };
+?> 
