1ea9fa512b2e76f179aecf0285e39039c52d8e81
Benita Almroth <benita.almroth@hotmail.com>
2025-12-30 11:55:34 +0100

    fixed: pathing issues and database error. booking works on one.com

--- app/config.php
+++ app/config.php
@@ -1,14 +1,13 @@
 <?php
-
 declare(strict_types=1);
 
 
-$base_url = ($_SERVER['HTTP_HOST'] === 'localhost:8000' || $_SERVER['HTTP_HOST'] === 'localhost') ? '' : '/yrgopelago';
+$isLocal = $_SERVER['HTTP_HOST'] === 'localhost:8000' || $_SERVER['HTTP_HOST'] === 'localhost';
+$base_url = $isLocal ? '' : '/yrgopelago';
 
 return [
     'title' => 'Project Name',
     'database_path' => sprintf('sqlite:%s/database/yrgopelago.db', __DIR__),
     'base_url' => $base_url,
 ];
-
 ?>
